a93f611307b028d42be554d42030ff3d018a439d
Will Spencer <willspencer171@gmail.com>
2023-10-16 20:27:40 +0100

    Updated Settings

--- Frameworks/Synchronous/Django/storefront/storefront/settings.py
+++ Frameworks/Synchronous/Django/storefront/storefront/settings.py
@@ -27,13 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'NewApp',
@@ -48,6 +48,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 ROOT_URLCONF = 'storefront.urls'
