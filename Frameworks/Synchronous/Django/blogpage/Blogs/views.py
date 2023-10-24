a7632bf9230450866b58794e5380f642f90476c4
Will Spencer <willspencer171@gmail.com>
2023-10-24 18:19:14 +0100

    Changed render method in homepage view

--- Frameworks/Synchronous/Django/blogpage/Blogs/views.py
+++ Frameworks/Synchronous/Django/blogpage/Blogs/views.py
@@ -4,10 +4,9 @@ from django.template import loader
 
 # Create your views here.
 def homepage(request):
-    template = loader.get_template("index.html")
     current_user = request.user
     context = {
         "user": current_user
     }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, "index.html", context)
