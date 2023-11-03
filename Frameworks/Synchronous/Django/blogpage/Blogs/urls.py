e6d4c109fe64f4f6b909532cd7b67f571f7ff773
Will Spencer <willspencer171@gmail.com>
2023-11-03 16:50:29 +0000

    Added urlpattern for viewing blogs with post ID number

--- Frameworks/Synchronous/Django/blogpage/Blogs/urls.py
+++ Frameworks/Synchronous/Django/blogpage/Blogs/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "Blogs"
 urlpatterns = [
     path("", views.homepage, name="home"),
+    path("blogs/<int:post_id>", views.view_blog, name="blog")
 ]
