800cd76235b64a138d1441bb852c301a95070aa8
Will Spencer <willspencer171@gmail.com>
2023-11-05 10:56:10 +0000

    Added urlpattern for blog creation

--- Frameworks/Synchronous/Django/blogpage/Blogs/urls.py
+++ Frameworks/Synchronous/Django/blogpage/Blogs/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "Blogs"
 urlpatterns = [
     path("", views.homepage, name="home"),
-    path("blogs/<int:post_id>", views.view_blog, name="blog")
+    path("blogs/<int:post_id>", views.view_blog, name="blog"),
+    path("createblog", views.create_blog)
 ]
