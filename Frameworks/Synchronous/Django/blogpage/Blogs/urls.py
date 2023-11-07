b1c4d9ffcbb8e9bf51b3228694b4be3367d32b69
Will Spencer <willspencer171@gmail.com>
2023-11-07 12:03:30 +0000

    Added path for login_user view

--- Frameworks/Synchronous/Django/blogpage/Blogs/urls.py
+++ Frameworks/Synchronous/Django/blogpage/Blogs/urls.py
@@ -6,5 +6,6 @@ app_name = "Blogs"
 urlpatterns = [
     path("", views.homepage, name="home"),
     path("blogs/<int:post_id>", views.view_blog, name="blog"),
-    path("createblog", views.create_blog)
+    path("createblog", views.create_blog),
+    path("login/", views.login_user, name="login_authorisation"),
 ]
