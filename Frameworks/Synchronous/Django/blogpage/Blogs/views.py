d7897c3d8ee7f774b65f73d47a5128b39f7a6889
Will Spencer <willspencer171@gmail.com>
2023-11-07 11:59:21 +0000

    Added view to process user logins

--- Frameworks/Synchronous/Django/blogpage/Blogs/views.py
+++ Frameworks/Synchronous/Django/blogpage/Blogs/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView
 from .models import *
 
 # Create your views here.
@@ -28,3 +30,17 @@ def create_blog(request):
     }
     
     return render(request, "create_blog.html", context)
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect("Blogs:home")
+    else:
+        return LoginView.as_view(extra_context={"error_message": True})
+            
+def logout_user(request):
+    pass
